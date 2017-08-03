80247ee9b4ec8a9835d36cbe74fae67d971c0a55
SadleyButton <brad.kh.sutton@gmail.com>
2017-08-03 11:39:39 -0400

    Inital Commit for Windows Form Application - Assignment 5 (BMI Calculator)

---
+++ COMP123-S2017-Assignment5/BMICalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Name: Bradley Sutton
+ * Date: August 03, 2017
+ * Student Number: 300280496
+ * GitHub: https://github.com/SadleyButton/COMP123-S2017/Assignment5
+ * Description - This is the Form for the BMI Calculator Windows Form Application
+ * Version 0.1
+ */
+
+namespace COMP123_S2017_Assignment5
+{
+    public partial class BMICalculator : Form
+    {
+        public BMICalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- COMP123-S2017-Assignment5/Program.cs
+++ COMP123-S2017-Assignment5/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+/* Name: Bradley Sutton
+ * Date: August 03, 2017
+ * Student Number: 300280496
+ * GitHub: https://github.com/SadleyButton/COMP123-S2017/Assignment5
+ * Description - This is the Driver Class for the BMI Calculator Windows Form Application - COMP123 Assignment 5
+ * Version 0.1
+ */
+
 namespace COMP123_S2017_Assignment5
 {
     static class Program
@@ -16,7 +25,7 @@ namespace COMP123_S2017_Assignment5
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BMICalculator());
         }
     }
 }
